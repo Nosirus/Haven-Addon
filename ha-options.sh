af106b4cfdebd249af794f0b6c00feb4bb5ae9cd
Nosirus <nosirus@gmail.com>
2026-07-22 18:58:33 +0200

    Update ha-options.sh

--- ha-options.sh
+++ ha-options.sh
@@ -2,8 +2,6 @@
 if [ -f /data/options.json ]; then
   NAME=$(sed -n 's/.*"server_name"[[:space:]]*:[[:space:]]*"\([^"]*\)".*/\1/p' /data/options.json)
   ADMIN=$(sed -n 's/.*"admin_username"[[:space:]]*:[[:space:]]*"\([^"]*\)".*/\1/p' /data/options.json)
-  PUBURL=$(sed -n 's/.*"public_url"[[:space:]]*:[[:space:]]*"\([^"]*\)".*/\1/p' /data/options.json)
   [ -n "$NAME" ] && export SERVER_NAME="$NAME"
   [ -n "$ADMIN" ] && export ADMIN_USERNAME="$ADMIN"
-  [ -n "$PUBURL" ] && export PUBLIC_URL="$PUBURL"
 fi
